24c106053804adaff27ea186ec8e84e03887311b
hicham346 <53907451+hicham346@users.noreply.github.com>
2019-08-22 13:19:41 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("614051062063824918")
+setInterval(function() {
+channel.send(`hicham is the pro player ever`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
